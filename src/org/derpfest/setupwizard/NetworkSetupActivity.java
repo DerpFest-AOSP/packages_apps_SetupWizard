LineageOS/android_packages_apps_SetupWizard

Optimize imports
Change-Id: I8e47e86d7c92a7e1a824cf0c052ac9eb9d1a3605

src/org/lineageos/setupwizard/NetworkSetupActivity.java
@@ -22,12 +22,10 @@ import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_ENABLE_NEXT_ON_CONN
 import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_PREFS_SET_BACK_TEXT;
 import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_PREFS_SHOW_BUTTON_BAR;
 import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_PREFS_SHOW_SKIP;
-import static org.lineageos.setupwizard.SetupWizardApp.REQUEST_CODE_SETUP_NETWORK;
 import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_PREFS_SHOW_SKIP_TV;
+import static org.lineageos.setupwizard.SetupWizardApp.REQUEST_CODE_SETUP_NETWORK;
 
 import android.content.Intent;
-
-import com.google.android.setupcompat.util.WizardManagerHelper;
 
 import org.lineageos.setupwizard.util.SetupWizardUtils;
 

DerpFest-AOSP/packages_apps_SetupWizard

src/org/derpfest/setupwizard/NetworkSetupActivity.java
@@ -22,12 +22,10 @@ import static org.derpfest.setupwizard.SetupWizardApp.EXTRA_ENABLE_NEXT_ON_CONNE
 import static org.derpfest.setupwizard.SetupWizardApp.EXTRA_PREFS_SET_BACK_TEXT;
 import static org.derpfest.setupwizard.SetupWizardApp.EXTRA_PREFS_SHOW_BUTTON_BAR;
 import static org.derpfest.setupwizard.SetupWizardApp.EXTRA_PREFS_SHOW_SKIP;
-import static org.derpfest.setupwizard.SetupWizardApp.REQUEST_CODE_SETUP_NETWORK;
 import static org.derpfest.setupwizard.SetupWizardApp.EXTRA_PREFS_SHOW_SKIP_TV;
+import static org.derpfest.setupwizard.SetupWizardApp.REQUEST_CODE_SETUP_NETWORK;
 
 import android.content.Intent;
-
-import com.google.android.setupcompat.util.WizardManagerHelper;
 
 import org.derpfest.setupwizard.util.SetupWizardUtils;
 